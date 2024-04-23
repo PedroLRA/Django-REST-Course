8f35766e50a7ca355bd509b5208b0f3211799e61
PedroLRA <pedro.lralm@gmail.com>
2024-04-23 04:04:01 +0100

    Initial Commit

---
+++ apps/school/views.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+
+def student(request):
+    if request.method == 'GET':
+        student = {
+                'id': 1,
+                'name': 'Guilherme'
+        }
+        return JsonResponse(student)
+
