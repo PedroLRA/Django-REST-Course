4d06113c009c5cef5f70db0b10001bdca00753e5
PedroLRA <pedro.lralm@gmail.com>
2024-04-23 04:45:25 +0100

    feat: Created Admin page CRUD

--- apps/school/admin.py
+++ apps/school/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from apps.school.models import Student, Course
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+    list_display = ('id', 'name', 'doc_rg', 'doc_cpf', 'birth')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 20
+
+admin.site.register(Student, Students)
+
+class Courses(admin.ModelAdmin):
+    list_display = ('id', 'code', 'description', 'level')
+    list_display_links = ('id', 'code')
+    search_fields = ('id', 'code')
+    list_per_page = 20
+
+admin.site.register(Course, Courses)
