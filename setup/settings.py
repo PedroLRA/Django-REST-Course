363c01cfd20525b2f31c80cd5247cfbaf341ff2b
PedroLRA <pedro.lralm@gmail.com>
2024-04-23 04:32:32 +0100

    fix: Fixing installed apps and paths

--- setup/settings.py
+++ setup/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.school.apps.SchoolConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
