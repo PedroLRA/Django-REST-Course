9c65c0935102e8f0526c701e1b30719697332c39
PedroLRA <pedro.lralm@gmail.com>
2024-04-26 00:22:15 +0100

    feat: Added django rest pagination

--- setup/settings.py
+++ setup/settings.py
@@ -114,7 +114,11 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+
+    # Pagination
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
 
 # Internationalization
